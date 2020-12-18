53df89dd42af9eee21a36c33d2f6f5dcf436cee1
tauseef <tau.leuven@gmail.com>
2020-12-18 09:32:50 +0100

    Take care of duplicates in both arrays

--- task.js
+++ task.js
@@ -7,19 +7,49 @@ const moment = MomentRange.extendMoment(Moment);
 
 //blank arrays to hold results of searching operations
 let updateExistingLst = [];
-let deleteExistingList = [];
 let delOverlappingExistingRels =[];
 
 // Existing and Desired Relations Array
 const existingRels = [];
 const desiredRels = [];
 
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
+function arraycomparer(otherArray){
+    return function(current){
+      return otherArray.filter(function(other){
+        return other.startDate == current.startDate && other.endDate == current.endDate
+      }).length == 0;
+    }
+  }
+
+
 function generateRelsForAPI(desiredRels,existingRels){
     console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>generateRelsForAPI<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
     //Lets try to take simple examples of date ranges and update counts and store operations in a result array
     let resultArray = [];
+    let deleteExistingList = [];
+
     let desiredRels_ovrlpIDs = desiredRels.map(el => ({ startDate:el.startDate, endDate:el.endDate, ovrID:0}))
     console.log(desiredRels_ovrlpIDs)
+
+    const desiredRels_ovrlpIDs_uniq = desiredRels_ovrlpIDs.filter(arraycomparer(existingRels));
+    const existingRels_uniq = existingRels.filter(arraycomparer(desiredRels_ovrlpIDs));
+
+
 
     //First we check for non overlapping DELETE scenarios and update resultArray
     //Strategy : Take each el of existing and compare with desired.
@@ -27,7 +57,7 @@ function generateRelsForAPI(desiredRels,existingRels){
     let overlapStatus = false; //start with no overlap initially
     //https://codeburst.io/comparison-of-two-arrays-using-javascript-3251d03877fe
     
-    existingRels.forEach(e1 =>{ desiredRels.forEach(e2 => 
+    existingRels_uniq.forEach(e1 =>{ desiredRels_ovrlpIDs_uniq.forEach(e2 => 
         {
                if( moment.range(e1.startDate,e1.endDate).overlaps(moment.range(e2.startDate,e2.endDate)) == true) {
                   console.log("First looping : Moment overlap detected");
@@ -47,14 +77,14 @@ function generateRelsForAPI(desiredRels,existingRels){
     });
 
     //Now mark for UPDATES and overlapping DELETEs
-    desiredRels_ovrlpIDs.forEach(e1 =>{ existingRels.forEach(e2 => 
+    desiredRels_ovrlpIDs_uniq.forEach(e1 =>{ existingRels_uniq.forEach(e2 => 
         {//We have marked ovrID 0 for each element in desiredREls. Lets check if element of desiredREls has overlap
             //If it has overlap then check if overID. if overID is 0 then first time overlap occurs- chng ovrID and mark for overlap
             //If despite overlap, ovrID is non zero then it means not first time overlap -> element of existingRel must be deleted
-            console.log("Second looping : Mark for updates: Run thru item des e1");
-            console.log(e1);
-            console.log("with existing e1");
-            console.log(e2);
+            console.log("Second looping : Checking if any updates: Run thru item des e1");
+            //console.log(e1);
+            //console.log("with existing e1");
+            //console.log(e2);
 
               if(     moment.range(e1.startDate,e1.endDate).overlaps(moment.range(e2.startDate,e2.endDate)) == true) {
                   if(e1.ovrID == 0){//mark for update for el in Desired
@@ -82,14 +112,27 @@ function generateRelsForAPI(desiredRels,existingRels){
     }
     );
 
+    console.log("Both loops have ended. Lets check our result now...");
+    console.log(resultArray);
+
+    console.log("Both loops have ended. Lets check if any overlapping deletes are recorded...");
+    console.log(deleteExistingList);
+
+
     //Now use the updateExistingLst and mark overlapping ExistingRels DELETEs : use filter operation
     //These are DELETES from items of Existing which overlapped with an item of desired but that item of desired had already overlapped to mark UPDATE another item of existing
-    delOverlappingExistingRels = existingRels.filter(el=> deleteExistingList.includes(el.key));
+    delOverlappingExistingRels = existingRels_uniq.filter(el=> deleteExistingList.includes(el.key));
     delOverlappingExistingRels.forEach(e1=>{resultArray.push({operation:"DELETE", body:{key:e1.key, startDate:e1.startDate, endDate:e1.endDate}})})
 
     console.log("Dump result to be returned-------->");
-    console.log(resultArray);
-    return resultArray;
+    //console.log(resultArray);
+
+    let finalArray;
+    //finalArray = resultArray.sort(dynamicSort("operation"));
+    finalArray = resultArray.sort((a, b) => (`${a.operation},${a.body.key},${a.body.startDate}` < `${b.operation},${b.body.key},${b.body.startDate}` ? -1 : 1));
+    console.log(finalArray);
+    return finalArray;
+   
 }
 let retResult = generateRelsForAPI(desiredRels,existingRels);
 console.log("Final returned result object");
--- test/task-test-chai.js
+++ test/task-test-chai.js
@@ -75,26 +75,6 @@ it('UPDATE DELETE case 4 ', () => {
   
 });
 
-it('UPDATE DELETE case 5 ', () => {
-  expect(generateRelsForAPI([{startDate: "2010-01-01", endDate : "2015-01-01"}],[{key:1,startDate: "2000-01-01", endDate : "2011-01-01"},{key:2,startDate: "2013-01-01", endDate : "2021-01-01"}])).to.eql( [
-    {
-      operation: 'UPDATE',
-      body: { key:1,
-        startDate:'2010-01-01',
-        endDate: '2015-01-01'
-            }
-      },
-    {
-    operation: 'DELETE',
-    body: { key:2,
-      startDate:'2013-01-01',
-      endDate: '2021-01-01'
-          }
-    }
-  ]);
-  
-});
-
 //new test cases from Tauseef
 it('Combined CREATE UPDATE DELETE case 6 ', () => {
   expect(generateRelsForAPI(
@@ -107,6 +87,7 @@ it('Combined CREATE UPDATE DELETE case 6 ', () => {
      [{key:1,startDate: "2010-03-17", endDate : "2011-06-20"},
        //next three(2,3,4) keys let them overlap with one period of desired so that overlapping deletions occur
         {key:2,startDate: "2011-01-18", endDate : "2011-03-20"},
+        //keys 3 and 4 should be deleted
         {key:3,startDate: "2011-04-18", endDate : "2011-06-20"},
         {key:4,startDate: "2011-08-18", endDate : "2011-10-20"},
         //now back to standard overlapping
@@ -151,11 +132,78 @@ it('Combined CREATE UPDATE DELETE case 6 ', () => {
             operation: 'DELETE',
             body: { key: 4, startDate: '2011-08-18', endDate: '2011-10-20' }
           }
-      ]
+      ].sort((a, b) => (`${a.operation},${a.body.key},${a.body.startDate}` < `${b.operation},${b.body.key},${b.body.startDate}` ? -1 : 1))
          
   );
   
 });
 
+it('Combined Same start and end date not producing anything in output 7 ', () => {
+  expect(generateRelsForAPI(
+     [{startDate: "2010-03-17", endDate : "2011-06-20"},
+       //next three(2,3,4) keys let them overlap with one period of desired so that overlapping deletions occur
+        {startDate: "2011-01-18", endDate : "2011-03-20"},
+        //keys 3 and 4 should be deleted
+        {startDate: "2011-04-18", endDate : "2011-06-20"},
+        {startDate: "2011-08-18", endDate : "2011-10-20"},
+        //now back to standard overlapping
+        {startDate: "2011-11-17", endDate : "2011-12-20"},
+        //one range very far so that non overlapping deletion is tested
+        {startDate: "2021-04-17", endDate : "2021-01-01"}
+      ],
+     [{key:1,startDate: "2010-03-17", endDate : "2011-06-20"},
+       //next three(2,3,4) keys let them overlap with one period of desired so that overlapping deletions occur
+        {key:2,startDate: "2011-01-18", endDate : "2011-03-20"},
+        //keys 3 and 4 should be deleted
+        {key:3,startDate: "2011-04-18", endDate : "2011-06-20"},
+        {key:4,startDate: "2011-08-18", endDate : "2011-10-20"},
+        //now back to standard overlapping
+        {key:5,startDate: "2011-11-17", endDate : "2011-12-20"},
+        //one range very far so that non overlapping deletion is tested
+        {key:6,startDate: "2021-04-17", endDate : "2021-01-01"}
+      ])).to.eql(
+      [  ]
+         
+  );
+  
+});
+
+it('DUPLICATEs with CREATE and DELETE case 8 ', () => {
+  expect(generateRelsForAPI([
+    //Lets take first two items to be same 
+    {startDate: "2010-03-17", endDate : "2010-06-20"},
+    {startDate: "2015-03-17", endDate : "2015-06-20"},
+    //Now take one different:must be created
+    {startDate: "2020-03-17", endDate : "2020-06-20"},
+  
+    ],
+    [
+      //Lets take first two items to be same 
+      {key:1,startDate: "2010-03-17", endDate : "2010-06-20"},
+      {key:2,startDate: "2015-03-17", endDate : "2015-06-20"},
+      //Now take on different-must be deleted
+      {key:3,startDate: "2019-03-17", endDate : "2019-06-20"},
+
+    ])).to.eql( [
+    {
+      operation: 'CREATE',
+      body: {
+        startDate:'2020-03-17',
+        endDate: '2020-06-20'
+      }
+    },
+    {
+      operation: 'DELETE',
+      body: {
+        key:3,
+        startDate:'2019-03-17',
+        endDate: '2019-06-20'
+      }
+    }
+
+
+    ]);
+  
+});
 
 
